5c2ecc5df894bdf8f9cafece08c9ff5b3fe168b6
Matheus Sarquis <matheussarquis2@gmail.com>
2020-06-22 19:41:20 -0300

    Adição da arvore binaria

--- ATIVIDADE_1/VG.cs
+++ ATIVIDADE_1/VG.cs
@@ -1,4 +1,4 @@
-﻿using ATIVIDADE_1.Arvore_de_Pesquisa_Binaria;
+﻿using ATIVIDADE_1.Arvore_Binaria;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +9,6 @@ namespace ATIVIDADE_1
 {
     class VG
     {
-        public ArvoreBin arvore = new ArvoreBin();
+        ArvoreBin arvore = new ArvoreBin();
     }
 }
