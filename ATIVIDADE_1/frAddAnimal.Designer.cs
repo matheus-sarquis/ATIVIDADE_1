97d163883687a831feaa8d76856d96eec238a9a3
Lucas Frias <lucasfriasoliveira@hotmail.com>
2020-06-23 14:13:11 -0300

    adicao codigo btn adcionar animal

--- ATIVIDADE_1/frAddAnimal.Designer.cs
+++ ATIVIDADE_1/frAddAnimal.Designer.cs
@@ -39,17 +39,9 @@
             this.cbSexo = new System.Windows.Forms.ComboBox();
             this.nupIdade = new System.Windows.Forms.NumericUpDown();
             this.gpPadrao = new System.Windows.Forms.GroupBox();
-            this.gpMamifero = new System.Windows.Forms.GroupBox();
-            this.cbCorPelos = new System.Windows.Forms.ComboBox();
-            this.label10 = new System.Windows.Forms.Label();
-            this.gbAve = new System.Windows.Forms.GroupBox();
-            this.cbCorPena = new System.Windows.Forms.ComboBox();
-            this.label12 = new System.Windows.Forms.Label();
             this.btnAdcionar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nupIdade)).BeginInit();
             this.gpPadrao.SuspendLayout();
-            this.gpMamifero.SuspendLayout();
-            this.gbAve.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -127,7 +119,7 @@
             // 
             this.txtNome.Location = new System.Drawing.Point(63, 19);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(200, 20);
+            this.txtNome.Size = new System.Drawing.Size(340, 20);
             this.txtNome.TabIndex = 8;
             // 
             // txtData
@@ -135,7 +127,7 @@
             this.txtData.Location = new System.Drawing.Point(160, 45);
             this.txtData.Mask = "00/00/0000";
             this.txtData.Name = "txtData";
-            this.txtData.Size = new System.Drawing.Size(103, 20);
+            this.txtData.Size = new System.Drawing.Size(243, 20);
             this.txtData.TabIndex = 9;
             this.txtData.ValidatingType = typeof(System.DateTime);
             // 
@@ -147,7 +139,7 @@
             "Fêmea"});
             this.cbSexo.Location = new System.Drawing.Point(55, 71);
             this.cbSexo.Name = "cbSexo";
-            this.cbSexo.Size = new System.Drawing.Size(208, 21);
+            this.cbSexo.Size = new System.Drawing.Size(348, 21);
             this.cbSexo.TabIndex = 10;
             // 
             // nupIdade
@@ -155,7 +147,7 @@
             this.nupIdade.Location = new System.Drawing.Point(63, 98);
             this.nupIdade.Name = "nupIdade";
             this.nupIdade.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.nupIdade.Size = new System.Drawing.Size(200, 20);
+            this.nupIdade.Size = new System.Drawing.Size(340, 20);
             this.nupIdade.TabIndex = 11;
             // 
             // gpPadrao
@@ -170,68 +162,10 @@
             this.gpPadrao.Controls.Add(this.txtNome);
             this.gpPadrao.Location = new System.Drawing.Point(12, 69);
             this.gpPadrao.Name = "gpPadrao";
-            this.gpPadrao.Size = new System.Drawing.Size(269, 134);
+            this.gpPadrao.Size = new System.Drawing.Size(409, 134);
             this.gpPadrao.TabIndex = 14;
             this.gpPadrao.TabStop = false;
             this.gpPadrao.Text = "Padrão";
-            // 
-            // gpMamifero
-            // 
-            this.gpMamifero.Controls.Add(this.cbCorPelos);
-            this.gpMamifero.Controls.Add(this.label10);
-            this.gpMamifero.Location = new System.Drawing.Point(287, 69);
-            this.gpMamifero.Name = "gpMamifero";
-            this.gpMamifero.Size = new System.Drawing.Size(269, 54);
-            this.gpMamifero.TabIndex = 15;
-            this.gpMamifero.TabStop = false;
-            this.gpMamifero.Text = "Mamífero";
-            // 
-            // cbCorPelos
-            // 
-            this.cbCorPelos.FormattingEnabled = true;
-            this.cbCorPelos.Location = new System.Drawing.Point(111, 19);
-            this.cbCorPelos.Name = "cbCorPelos";
-            this.cbCorPelos.Size = new System.Drawing.Size(152, 21);
-            this.cbCorPelos.TabIndex = 17;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(6, 16);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(99, 23);
-            this.label10.TabIndex = 17;
-            this.label10.Text = "Cor dos pelos:";
-            // 
-            // gbAve
-            // 
-            this.gbAve.Controls.Add(this.cbCorPena);
-            this.gbAve.Controls.Add(this.label12);
-            this.gbAve.Location = new System.Drawing.Point(287, 129);
-            this.gbAve.Name = "gbAve";
-            this.gbAve.Size = new System.Drawing.Size(269, 53);
-            this.gbAve.TabIndex = 16;
-            this.gbAve.TabStop = false;
-            this.gbAve.Text = "Voa";
-            // 
-            // cbCorPena
-            // 
-            this.cbCorPena.FormattingEnabled = true;
-            this.cbCorPena.Location = new System.Drawing.Point(108, 19);
-            this.cbCorPena.Name = "cbCorPena";
-            this.cbCorPena.Size = new System.Drawing.Size(155, 21);
-            this.cbCorPena.TabIndex = 18;
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(6, 17);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(96, 23);
-            this.label12.TabIndex = 17;
-            this.label12.Text = "Cor da pena:";
             // 
             // btnAdcionar
             // 
@@ -241,7 +175,7 @@
             this.btnAdcionar.ForeColor = System.Drawing.Color.White;
             this.btnAdcionar.Location = new System.Drawing.Point(12, 209);
             this.btnAdcionar.Name = "btnAdcionar";
-            this.btnAdcionar.Size = new System.Drawing.Size(544, 40);
+            this.btnAdcionar.Size = new System.Drawing.Size(409, 40);
             this.btnAdcionar.TabIndex = 17;
             this.btnAdcionar.Text = "Adcionar";
             this.btnAdcionar.UseVisualStyleBackColor = false;
@@ -252,10 +186,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(567, 273);
+            this.ClientSize = new System.Drawing.Size(436, 273);
             this.Controls.Add(this.btnAdcionar);
-            this.Controls.Add(this.gbAve);
-            this.Controls.Add(this.gpMamifero);
             this.Controls.Add(this.gpPadrao);
             this.Controls.Add(this.cbAnimal);
             this.Controls.Add(this.label1);
@@ -264,10 +196,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.nupIdade)).EndInit();
             this.gpPadrao.ResumeLayout(false);
             this.gpPadrao.PerformLayout();
-            this.gpMamifero.ResumeLayout(false);
-            this.gpMamifero.PerformLayout();
-            this.gbAve.ResumeLayout(false);
-            this.gbAve.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -286,12 +214,6 @@
         private System.Windows.Forms.ComboBox cbSexo;
         private System.Windows.Forms.NumericUpDown nupIdade;
         private System.Windows.Forms.GroupBox gpPadrao;
-        private System.Windows.Forms.GroupBox gpMamifero;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.GroupBox gbAve;
-        private System.Windows.Forms.ComboBox cbCorPelos;
-        private System.Windows.Forms.ComboBox cbCorPena;
-        private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Button btnAdcionar;
     }
 }
